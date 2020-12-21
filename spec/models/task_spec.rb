6b241851998e2cd273d3ae4b32be5331fd6f4523
nobita <inouetakahiro163@gmail.com>
2020-12-21 11:48:55 +0900

    test: titleがなければ無効

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'is valid with all attributes' do
-    task = FactoryBot.create(:task)
-    expect(task).to be_valid
+  describe 'validation' do
+    it 'is valid with all attributes' do
+      task = FactoryBot.create(:task)
+      expect(task).to be_valid
+    end
+
+    it 'is invalid without title' do 
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    
   end
 end
