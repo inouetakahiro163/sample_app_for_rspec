a5ce0448f26ba0f9af6c00853b5e5f95bb07df71
nobita <inouetakahiro163@gmail.com>
2020-12-21 12:54:15 +0900

    taskタイトルsequenceに変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
-    title {"タイトル"}
-    content {"コンテント"}
-    status {0}
-    deadline {"2020-12-25 15:10:00"}
+    sequence(:title, "title_1")
+    content { "コンテント" }
+    status { :todo }
+    deadline { 1.week.from_now}
     association :user
   end
 end
