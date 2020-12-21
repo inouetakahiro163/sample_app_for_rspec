f699de1c0a09fa4b577a92be6d58497cbc2d8095
nobita <inouetakahiro163@gmail.com>
2020-12-21 11:10:47 +0900

    sampleデータ作成とタスクが有効かのテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    title {"タイトル"}
+    content {"コンテント"}
+    status {0}
+    deadline {"2020-12-25 15:10:00"}
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with all attributes' do
+    task = FactoryBot.create(:task)
+    expect(task).to be_valid
+  end
 end
