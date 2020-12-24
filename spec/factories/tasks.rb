f94a834d6605855f21d4cff5615a210512242257
takahiro.inoue <71106047+inouetakahiro163@users.noreply.github.com>
2020-12-24 09:15:05 +0900

    Revert "02 model spec"

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :task do
-    sequence(:title, "title_1")
-    content { "コンテント" }
-    status { :todo }
-    deadline { 1.week.from_now}
-    association :user
+    
   end
 end
--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    sequence(:email){ |n| "user_#{n}@example.com" }
-    password {"maria163"}
-    password_confirmation {"maria163"}
-  end
-end
