46a0fa6a80084389b7c43bf2a7b05352094be330
nobita <inouetakahiro163@gmail.com>
2020-12-31 09:29:07 +0900

    test: tasks_spec タスクスペックの6つのテスト大枠作成 ログイン前は新規作成ページに遷移できない

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
+  let(:task){ create(:task) }
+  describe 'ログイン前' do
+    describe 'タスクの新規作成' do
+      context 'ログインしていない場合' do
+        it 'タスクの新規作成ができない' do
+          visit new_task_path
+          expect(page).to have_content('Login required')
+          expect(current_path).to eq login_path
+        end
+      end
+    end
+
+    describe 'タスクの編集' do
+      context 'ログインしていない場合' do
+        it 'タスクの編集ができない'
+
+      end
+    end
+
+    describe 'マイページへの遷移' do
+      context 'ログインしていない場合' do
+        it 'マイページへの遷移ができない'
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    describe 'タスクの新規作成' do
+      context 'ログインした場合' do
+        it 'タスクの新規作成ができる'
+      end
+    end
+
+    describe 'タスクの編集' do
+      context 'ログインした場合' do
+        it 'タスクの編集ができる'
+      end
+    end
+
+    describe 'タスクの削除' do
+      context 'ログインした場合' do
+        it 'タスクの削除ができる'
+      end
+    end
+  end
 end
