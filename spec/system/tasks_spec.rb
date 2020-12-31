2cbb9f15de318255eb6a2a645002ed13b96eb8e1
nobita <inouetakahiro163@gmail.com>
2020-12-31 11:06:17 +0900

    test: タスクの新規作成ができる

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
+  include LoginSupport
+  let(:user){ create(:user) }
   let(:task){ create(:task) }
+
   describe 'ログイン前' do
     describe 'タスクの新規作成' do
       context 'ログインしていない場合' do
@@ -25,15 +28,31 @@ RSpec.describe "Tasks", type: :system do
   end
 
   describe 'ログイン後' do
+    before{ login_as(user) }
     describe 'タスクの新規作成' do
       context 'ログインした場合' do
-        it 'タスクの新規作成ができる'
+        it 'タスクの新規作成ができる' do
+          visit new_task_path
+          fill_in 'Title', with: 'test'
+          fill_in 'Content', with: 'test_content'
+          select 'doing', from: 'Status'
+          fill_in 'Deadline', with: DateTime.new(2020, 12, 31, 12, 30)
+          click_button 'Create Task'
+          expect(page).to have_content 'test'
+          expect(page).to have_content 'test_content'
+          expect(page).to have_content 'doing'
+          expect(page).to have_content '2020/12/31 12:30'
+          expect(current_path).to eq '/tasks/1'
+        end
       end
     end
 
     describe 'タスクの編集' do
+      let!(:task){ create(:task, user: user) }
       context 'ログインした場合' do
-        it 'タスクの編集ができる'
+        it 'タスクの編集ができる' do
+          visit 
+        end
       end
     end
 
